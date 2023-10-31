8e825a17a729e0de9cec6ef1c83376bfb6c35b12
lucas pedroso <pedrosolucas1745@gmail.com>
2023-10-31 10:13:39 -0300

    subindo modificações no BD

--- banco de dados/db_integreer_project.sql
+++ banco de dados/db_integreer_project.sql
@@ -6,54 +6,59 @@ id_game_microsoft int not null primary key auto_increment,
 id_company_microsoft int not null,
 game_name varchar(80) not null,
 Description_game varchar(4000),
+link_video varchar(200),
 oficial_price double null,
-ofical_date date not null
+ofical_date date null
 );
-create table Sony(
+create table sony(
 id_game_sony int not null primary key auto_increment,
 id_company_sony int not null,
 game_name varchar(80) not null,
 Description_game varchar(4000),
+link_video varchar(200),
 oficial_price double  null,
-ofical_date date not null
+ofical_date date  null
 );
-create table Nintendo(
+create table nintendo(
 id_game_nintendo int not null primary key auto_increment,
 id_company_nintendo int not null,
 game_name varchar(80) not null,
 Description_game varchar(4000),
+link_video varchar(200),
 oficial_price double  null,
-ofical_date date not null
+ofical_date date  null
 );
-create table PC(
+create table pc(
 id_game_pc int not null primary key auto_increment,
 id_company_pc int not null,
 game_name varchar(80) not null,
 Description_game varchar(4000),
+link_video varchar(200),
 oficial_price double null,
-ofical_date date not null
+ofical_date date null
 );
-create table Game_not_exclusive(
+create table game_not_exclusive(
 id_game int not null auto_increment primary key,
 game_name varchar(80) not null,
 Description_game varchar(4000),
+link_video varchar(200),
 oficial_price double  null,
-ofical_date date not null,
+ofical_date date  null,
 plataform_microsoft boolean not null,
 plataform_sony boolean not null,
 plataform_nintendo boolean not null,
 plataform_pc boolean not null
 );
-create table dataEventos(
-id_data int not null primary key,
+create table data_eventos(
+id_data int not null auto_increment primary key,
 id_empresa int not null,
 lastEvent date,
 nextevent date
 );
 
-insert into microsoft values(null,1,"1 teste de jogo para a requisição 1","descrição para jogo da requisição",199.99,'2023-05-15'),
-                            (null,1,"2 teste de jogo para a requisição 2","descrição para jogo da requisição",199.99,'2023-05-15'),
-                            (null,1,"3 teste de jogo para a requisição 3","descrição para jogo da requisição",199.99,'2023-05-15');
+insert into microsoft values(null,1,"1 teste de jogo para a requisição 1","descrição para jogo da requisição","https://www.youtube.com/watch?v=JVrZVdEnxHE",199.99,'2023-05-15'),
+                            (null,1,"2 teste de jogo para a requisição 2","descrição para jogo da requisição","https://www.youtube.com/watch?v=JVrZVdEnxHE",199.99,'2023-05-15'),
+                            (null,1,"3 teste de jogo para a requisição 3","descrição para jogo da requisição","https://www.youtube.com/watch?v=JVrZVdEnxHE",199.99,'2023-05-15');
 
 insert into microsoft values
  (1,1,"Postal: Brain Damaged",": POSTAL: Brain Damaged é uma nova versão orientada para a ação da série de jogos cult Running With Scissors com gráficos de retrocesso e jogabilidade moderna e em ritmo acelerado. Faça um passeio de montanha-russa selvagem pelo país das maravilhas retorcido envolto na mente confusa do POSTAL DUDE",null),
@@ -70,7 +75,7 @@ insert into Game_not_exclusive values (null,"Postal: Brain Damaged"," Brain Dama
 									  (null,"Sonic Origins",":Tenha uma nova experiência das aventuras clássicas de Sonic The Hedgehog, Sonic The Hedgehog 2, Sonic 3 & Knuckles e Sonic CD no Sonic Origins remasterizado! Da emblemática Zona Green Hill ao traiçoeiro Robô Death Egg, você vai percorrer uma infinidade de momentos memoráveis na sua missão para frustrar os planos do Dr. Robotnik em alta definição! Esta versão atualizada contém áreas inéditas, animações adicionais e um novo Modo Aniversário!",225,"2022-06-22",5,5,5,5);
 
 
-insert into dataEventos values (null,1,'2023-02-20','2023-05-15');
+insert into data_eventos values (null,1,'2023-02-20','2023-05-15');
 drop database db_itegreer_project;						
 
 
