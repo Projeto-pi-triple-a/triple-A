1f029f252a9bce991b743b452dec718cc7e66c0d
lucas pedroso <pedrosolucas1745@gmail.com>
2023-11-04 00:15:07 -0300

    add dependencia cors para liberar requisições

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const router = require('./router')
 const app = express();
 const port = process.env.port
+app.use(cors())
 app.use(express.json())
 app.use(router)
 app.listen(port,()=>{console.log(`server running at port ${port}`)})
