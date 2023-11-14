06195e12d432c7aab583729c249a82241bf223e6
lucas pedroso <pedrosolucas1745@gmail.com>
2023-11-14 11:58:03 -0300

    subindo post de games não exclusivos e alteração para limpar fomulário

--- frontend/JS/notExclusiveGames.js
+++ frontend/JS/notExclusiveGames.js
@@ -1,4 +1,5 @@
 
+
 const btnSubmit = document.querySelector('#send')
 btnSubmit.addEventListener('click', function () {
     SendGame(event);
@@ -7,22 +8,30 @@ btnSubmit.addEventListener('click', function () {
 async function SendGame (event)
 {
     event.preventDefault();
+   
+    const microsoft = document.querySelector('#microsoft').checked ? 1 : 0;
+    const sony = document.querySelector('#sony').checked ? 1 : 0;
+    const nintendo = document.querySelector('#nintendo').checked ? 1 : 0;
+    const pc = document.querySelector('#pc').checked ? 1 : 0;
     const gameName = document.querySelector('#gameName').value
     const descriptionGame = document.querySelector('#descriptionGame').value
     const linkVideo = document.querySelector('#linkVideo').value
     const priceGame = document.querySelector('#priceGame').value
     const dateGame = document.querySelector('#dateGame').value
-    const company = document.querySelector('#company').value
     const game = 
     {
-        company : company,
         gameName : gameName,
         descriptionGame : descriptionGame,
         linkVideo : linkVideo,
         oficialPrice : priceGame,
-        oficialDate : dateGame
+        oficialDate : dateGame,
+        plataformMicrosoft :microsoft,
+        plataformSony :sony,
+        plataformNintendo :nintendo,
+        plataformPc:pc
     }
-    const response = await fetch('http://localhost:3000/games',
+    console.log(game)
+    const response = await fetch('http://localhost:3000/gamesNotExclusive',
     {
         method : 'POST',
         body : JSON.stringify(game),
@@ -35,12 +44,18 @@ async function SendGame (event)
     }
     else
     {
-        data.insertId
-        alert(`game ID ${data.insertId} inserido com sucesso na plataforma ${company}`)
+        data.insertId;
+        alert(`Game ID ${data.insertId} inserido com sucesso`);
+        document.querySelector('#microsoft').checked = false;
+        document.querySelector('#sony').checked = false;
+        document.querySelector('#nintendo').checked = false;
+        document.querySelector('#pc').checked = false;
         document.querySelector('#gameName').value = "";
         document.querySelector('#descriptionGame').value = "";
         document.querySelector('#linkVideo').value = "";
         document.querySelector('#priceGame').value = "";
         document.querySelector('#dateGame').value = "";
+        
+    
     }
 }
